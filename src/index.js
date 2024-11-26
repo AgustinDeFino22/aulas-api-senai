129b6d2b8c1eb264bbfd8aafc5786d3118771d8f
Agustin <agustindefino19@gmail.com>
2024-11-26 14:53:01 -0300

    exercícios de fixação

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+import UsuariosController from "./controllers/UsuariosController.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Rotas de /usuarios
+const _usuariosController = new UsuariosController();
+
+app.get("/usuarios", _usuariosController.listar);
+app.post("/usuarios", _usuariosController.adicionar);
+app.put("/usuarios", _usuariosController.atualizar);
+app.delete("/usuarios/:id", _usuariosController.excluir);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`API está rodando na porta ${port}`);
+});
